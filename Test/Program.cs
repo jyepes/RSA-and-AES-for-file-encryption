136d00cf3faa03daace21dd297c1d02ac9f70868
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-06-09 09:46:49 -0500

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Encryptor;
+using FileEncryptor;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GenerateRSAKeyPair();
+
+        }
+
+        private static void GenerateRSAKeyPair()
+        {
+            string publicKey = string.Empty;
+            string privateKey = string.Empty;
+            Encipher.GenerateRSAKeyPair(out publicKey, out privateKey);
+        }
+    }
+}
